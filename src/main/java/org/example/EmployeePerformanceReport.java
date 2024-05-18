0eda56e7b11bd48a5fd06dd11d6fe1fbb0686059
raghu <dev.raghu95@gmail.com>
2024-05-18 19:50:37 +0530

    implemented the logic

--- src/main/java/org/example/EmployeePerformanceReport.java
+++ src/main/java/org/example/EmployeePerformanceReport.java
@@ -12,20 +12,35 @@ record Employee(int id,String name){}
 @JsonIgnoreProperties(ignoreUnknown = true)
 record EmployeeRatings(int employeeId, double rating, int quarter){}
 
+//Finding top3 employees by average rating
 public class EmployeePerformanceReport {
 
     public static void main(String[] args) {
+
+        record EmployeeAverageRating(Employee employee, double rating){}
+
         try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");) {
             MongoDataFetcher dataFetcher = new MongoDataFetcher(mongoClient);
             List<Employee> employees = dataFetcher.getData("employee", Employee.class);
             List<EmployeeRatings> employeeRatings = dataFetcher.getData("employee-ratings", EmployeeRatings.class);
 
-
+            //grouping the rating by employeeId
+          Map<Integer, List<Double>> employeeRatingMap =   employeeRatings.stream().collect(Collectors.groupingBy(
+                    EmployeeRatings::employeeId,Collectors.mapping(EmployeeRatings::rating, Collectors.toList())
+            ));
+        //finding out average rating and return result in the form of local record
+         List<EmployeeAverageRating> employeeAverageRatingList =    employees.stream().map(employee -> {
+               double rating =  employeeRatingMap.
+                       getOrDefault(employee.id(), List.of()).stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+               return new EmployeeAverageRating(employee, rating);
+            }).toList();
+         //filtering out top 3 employees
+            employeeAverageRatingList.stream().sorted(Comparator.comparing(EmployeeAverageRating::rating).reversed())
+                    .limit(3)
+                    .forEach(System.out::println);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 }
